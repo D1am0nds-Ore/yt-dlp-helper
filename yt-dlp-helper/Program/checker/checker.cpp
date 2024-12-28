59492303b4df710d7fef10445a9e0195a39e4d76
D_Ore <D1am0nds_Ore@outlook.com>
2024-12-28 22:30:02 +0800

    add more python version check

--- yt-dlp-helper/Program/checker/checker.cpp
+++ yt-dlp-helper/Program/checker/checker.cpp
@@ -4,5 +4,7 @@ using namespace std;
 bool PyChecker::isPyInstalled() {
 	int result = system("python --version >null 2>&1");
 	if (result == 0) return true;
-else return false;
+	result = system("python3 --version >null 2>&1");
+	if (result == 0) return true;
+	return false;
 }
